21ba96d045e8ba870929ce7f8a901aa7e6a91167
Jake Spurlock <whyisjake@gmail.com>
2022-10-07 07:58:41 -0700

    Week 7

---
+++ fall-2022/week-7/footer.php
@@ -0,0 +1,16 @@
+ <!-- FOOTER -->
+ <footer class="container">
+      <p class="float-end"><a href="#">Back to top</a></p>
+      <p>&copy; 2017–2022 Kittens, Inc. &middot; <a href="#">Privacy</a> &middot; <a href="#">Terms</a></p>
+    </footer>
+  </main>
+
+
+  <script src="https://getbootstrap.com/docs/5.2/dist/js/bootstrap.bundle.min.js"
+    integrity="sha384-OERcA2EqjJCMA+/3y+gxIOqMEjwtxJY7qPCqsdltbNJuaOe923+mo//f6V8Qbsw3"
+    crossorigin="anonymous"></script>
+
+
+</body>
+
+</html>
